e7cd204ae9573508b9487ea09e1eaf9aa7405d33
simranbirla <simranbirla@Simrans-MacBook-Pro.local>
2025-02-08 11:57:56 +0530

    chore: modal css changes

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -3,16 +3,17 @@ import React from 'react'
 export default function Modal() {
     return (
         <div className='absolute top-0 left-0 w-[100vw] h-[100vh] flex justify-center items-center bg-gray-500 text-white text-2xl'>
-            <div className='absolute top-6 right-12 font-bold cursor-pointer'>
-                X
-            </div>
-            <div className='w-[80%] h-[80%] relative rounded-2xl'>
+
+            <div className='w-[85%] h-full relative rounded-2xl'>
+                <div className='absolute top-2 right-4 font-bold cursor-pointer'>
+                    X
+                </div>
                 <div className='flex gap-3 absolute top-2 left-2 items-center'>
                     <img src={'https://cdn.pixabay.com/photo/2023/01/18/10/39/ai-generated-7726481_1280.jpg'} className='rounded-full size-8' />
                     <p className='text-sm'>name</p>
                     <p className='text-sm text-gray-600 font-bold'>56 min</p>
                 </div>
-                <img src={'https://cdn.pixabay.com/photo/2023/01/18/10/39/ai-generated-7726481_1280.jpg'} className='w-full h-full rounded-2xl' alt="modal-image" />
+                <img src={'https://cdn.pixabay.com/photo/2023/01/18/10/39/ai-generated-7726481_1280.jpg'} className='w-full h-full rounded-xl' alt="modal-image" />
             </div>
         </div>
     )
--- src/Stories.tsx
+++ src/Stories.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import mockData from './data'
 import { TUserStory } from './types/Story';
 import UserStory from './UserStory';
+import Modal from './Modal';
 
 export default function Stories() {
     const [stories, setStories] = useState<TUserStory[]>(mockData);
@@ -11,6 +12,7 @@ export default function Stories() {
             {stories.map(s => {
                 return <div key={s.id}><UserStory {...s} /></div>
             })}
+            <Modal />
         </div>
     )
 }
